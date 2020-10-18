f6fcd24cf9c525571cbcb3e85036c1d7e73478a5
tiana <tiana.randrianarivony@gmail.com>
2020-10-18 15:54:05 +0300

    init

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+/**
+ * @author("tiana.randrianarivony@gmail.com")
+ */
+import React from "react";
+const Header = () => {
+    return (
+        <header>
+            <h1>App</h1>
+        </header>
+    )
+}
+
+export default Header;
